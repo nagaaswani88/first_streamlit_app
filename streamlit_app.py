cae79f05efd1ebeed9f996d67a09805a66b59545
nagaaswani88 <127345099+nagaaswani88@users.noreply.github.com>
2023-03-08 15:54:11 -0500

    Update streamlit_app.py
    added emojis for the menu.
    added own fruit receipe

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🫐🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬🥬Kale,Spinach and Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🍓Build your own Fruit Smoothie🥝🍇')
